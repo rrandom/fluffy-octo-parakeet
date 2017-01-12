2564ddd0b9df40e83d12c4a7bdfb28cc3b5f92c3
rrandom <emanonhere@qq.com>
2017-01-12 22:52:42 +0800

    create on object

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -1,7 +1,19 @@
 import _ from 'lodash'
 
-export function observe (value, asRootData) {
+export class Observer {
+
+  constructor (value) {
+    this.value = value
+    value.__ob__ = this    // value.__ob__ = this
+  }
+}
+
+export function observe (value) {
   if (!_.isObject(value)) {
     return
+  } else if (value.hasOwnProperty('__ob__') && value.__ob__ instanceof Observer) {
+    return value.__ob__
+  } else if (Object.isExtensible(value)) {
+    return new Observer(value)
   }
 }
--- test/observer.spec.js
+++ test/observer.spec.js
@@ -1,5 +1,5 @@
 
-import { observe } from 'core/observer/index'
+import { Observer, observe } from 'core/observer/index'
 
 describe('Observer', () => {
   it('create on non-observables', () => {
@@ -8,5 +8,19 @@ describe('Observer', () => {
 
     const ob3 = observe(Object.freeze({}))
     expect(ob3).toBeUndefined()
+  })
+
+  it('create on object', () => {
+    const obj = {
+      a: {},
+      b: {}
+    }
+    const ob1 = observe(obj)
+    expect(ob1 instanceof Observer).toBe(true)
+    expect(ob1.value).toBe(obj)
+    expect(obj.__ob__).toBe(ob1)
+
+    const ob2 = observe(obj)
+    expect(ob2).toBe(ob1)
   })
 })
