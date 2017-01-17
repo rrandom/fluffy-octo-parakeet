0475db782e005716ceb9137f387cac9db6bc9423
rrandom <emanonhere@qq.com>
2017-01-17 14:54:21 +0800

    create on already observed object

--- test/observer.spec.js
+++ test/observer.spec.js
@@ -36,4 +36,38 @@ describe('Observer', () => {
     const ob2 = observe(obj)
     expect(ob2).toBe(ob1)
   })
+
+  it('create on already observed object', () => {
+    const obj = {}
+    let val = 0
+    let getCount = 0
+    Object.defineProperty(obj, 'a', {
+      configurable: true,
+      enumerable: true,
+      get () {
+        getCount++
+        return val
+      },
+      set (v) { val = v }
+    })
+
+    const ob1 = observe(obj)
+    expect(ob1 instanceof Observer).toBe(true)
+    expect(ob1.value).toBe(obj)
+    expect(obj.__ob__).toBe(ob1)
+
+    getCount = 0
+
+    obj.a
+    expect(getCount).toBe(1)
+    obj.a
+    expect(getCount).toBe(2)
+
+    const ob2 = observe(obj)
+    expect(ob2).toBe(ob1)
+
+    obj.a = 10
+    expect(val).toBe(10)
+    console.log(obj.a)
+  })
 })
